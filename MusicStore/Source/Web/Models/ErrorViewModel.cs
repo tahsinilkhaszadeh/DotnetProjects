7a887391aa23ef0dcee6d1eba30c2a752e4869a2
Tahsin Ilkhas Zadeh <tahsinilkhaszadeh@gmail.com>
2025-03-21 14:25:00 -0700

    New project created
    New project created, sub directories created

---
+++ MusicStore/Source/Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MusicStore.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
